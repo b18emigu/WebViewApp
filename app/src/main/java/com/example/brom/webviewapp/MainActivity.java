4acabde3cec42623e432d30c0f4d67afab1810ec
b18emigu <b18emigu@student.his.se>
2019-04-08 11:49:59 +0200

    Switch between internal and external page.

--- app/src/main/java/com/example/brom/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/brom/webviewapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +15,10 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     // Create a private member variable that can hold our WebView
+    private WebView wv;
+    private String external_url = "http://wwwlab.iit.his.se/b18emigu/Mobilapplikationsdesign/Prototyp/index.html";
+    private String internal_url = "file:///android_asset/about.html";
+    private boolean isExternal = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +28,34 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        WebView wv = (WebView) findViewById(R.id.my_webview);
+        wv = (WebView) findViewById(R.id.my_webview);
         // WebView wv = new WebView(this);
         WebViewClient wvc = new WebViewClient();
         wv.setWebViewClient(wvc);
         WebSettings ws = wv.getSettings();
         ws.setJavaScriptEnabled(true);
+        wv.loadUrl(internal_url);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isExternal) {
+                    wv.loadUrl(internal_url);
+                    isExternal = false;
+                } else {
+                    wv.loadUrl(external_url);
+                    isExternal = true;
+                }
+            }
+        });
 
         //setContentView(wv);
 
         // External
         // wv.loadUrl("http://wwwlab.iit.his.se/b18emigu/Mobilapplikationsdesign/Prototyp/index.html");
         // Internal
-        wv.loadUrl("file:///android_asset/about.html");
-
+        // wv.loadUrl("file:///android_asset/about.html");
         // Internal website = file:///android_asset/
 
         // 1. Create a WebView element in the layout file content_main.xml
